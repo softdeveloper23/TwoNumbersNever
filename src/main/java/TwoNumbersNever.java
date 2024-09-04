9a7b98ddd3fc6f577c4fe668e6edf8946bb3fd92
softdeveloper23 <brannongarrett77@gmail.com>
2024-09-04 18:12:51 +0200

    Solved the exercise

--- src/main/java/TwoNumbersNever.java
+++ src/main/java/TwoNumbersNever.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class TwoNumbersNever {
     public static void main(String[] args) {
         // A single boolean value true if n and m never occur next to each other; otherwise, it is false
-        boolean didOccur = false;
+        boolean neverOccurs = true;
         // Create scanner
         Scanner scanner = new Scanner(System.in);
         // Read the size of the array
@@ -17,9 +17,15 @@ public class TwoNumbersNever {
         // Read two integers n and m
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+        // Compare elements of the array to see if n and m (or m and n) occur next to each other in the array
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] == n && array[i + 1] == m || array[i] == m && array[i + 1] == n) {
+                neverOccurs = false;
+                break;
+            }
         }
+        System.out.println(neverOccurs);
+
+        scanner.close();
     }
 }
