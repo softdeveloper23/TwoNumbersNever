919d438018432fa49a0b40b6261cf395fdc6ca47
softdeveloper23 <brannongarrett77@gmail.com>
2024-09-04 16:46:03 +0200

    Setup basic code

--- src/main/java/TwoNumbersNever.java
+++ src/main/java/TwoNumbersNever.java
@@ -2,6 +2,24 @@ import java.util.Scanner;
 
 public class TwoNumbersNever {
     public static void main(String[] args) {
-        // code
+        // A single boolean value true if n and m never occur next to each other; otherwise, it is false
+        boolean didOccur = false;
+        // Create scanner
+        Scanner scanner = new Scanner(System.in);
+        // Read the size of the array
+        int arraySize = scanner.nextInt();
+        // Initialize the array with the array size
+        int[] array = new int[arraySize];
+        // Read the array elements from user
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = scanner.nextInt();
+        }
+        // Read two integers n and m
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
